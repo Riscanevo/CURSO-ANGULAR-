491bde98b17f81327cf573c3bcddfc70e40dc357
Eduardo Riscanevo <fredyriscanevo1971@gmail.com>
2026-03-10 16:27:37 -0500

    class_two

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  
 })
 export class App {
-  protected readonly title = signal('Curso_Angular xd');
+  protected readonly title = signal('Eduardo!!! ');
 }
---
+++ src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,35 @@
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+
+@Component({
+templateUrl: './counter-page.component.html',
+styleUrls:['./counter-page.component.css'],
+changeDetection: ChangeDetectionStrategy.OnPush,
+
+})
+
+
+
+
+export class CounterPageComponent {
+counter = 10;
+counterSignal = signal(10);
+
+constructor() {
+  setInterval(() => {
+    this.increaseBy(1);
+    console.log('Tick');
+  },2000);
+}
+
+
+
+increaseBy(value: number){
+  this.counter += value;
+  // this.counterSignal.update(current => current + value);
+  this.counterSignal.update(current => current + value);
+}
+resetCounter(){
+  this.counter = 10;
+  this.counterSignal.set(10);
+}
+}
